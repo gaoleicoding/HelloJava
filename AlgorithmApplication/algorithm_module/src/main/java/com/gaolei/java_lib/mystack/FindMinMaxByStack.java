365bf7d519551b93f607d6618126df078629ff9c
GL <gl@GaoLeideMacBook-Pro.local>
2019-03-23 10:25:00 +0800

    实现一些算法

---
+++ AlgorithmApplication/algorithm_module/src/main/java/com/gaolei/java_lib/mystack/FindMinMaxByStack.java
@@ -0,0 +1,80 @@
+package com.gaolei.java_lib.mystack;
+
+import java.util.Stack;
+
+public class FindMinMaxByStack<E extends Comparable<E>> {
+    Stack<E> stack1 = new Stack<E>();
+    Stack<E> stackMin = new Stack<E>();//存放求最小值的栈
+    Stack<E> stackMax = new Stack<E>();//存放求最大值的栈
+
+    public static void main(String[] args) {
+        FindMinMaxByStack<Integer> demo08 = new FindMinMaxByStack();
+        demo08.push(1);
+        demo08.push(2);
+        demo08.push(3);
+        demo08.push(4);
+        demo08.push(3);
+        demo08.push(2);
+        System.out.println("min:" + demo08.getMin());
+        System.out.println("max:" + demo08.getMax());
+        System.out.println("pop" + demo08.pop());
+        System.out.println("max:" + demo08.getMax());
+        System.out.println("min:" + demo08.getMin());
+        demo08.push(5);
+        System.out.println("pop:" + demo08.pop());
+
+    }
+
+    public void push(E e) {
+        stack1.push(e);
+
+        if (stackMin.isEmpty() || e.compareTo(stackMin.peek()) < 0) {
+            stackMin.push(e);//若最小栈为空push进stack时就同时把它push进stackMin;
+//                System.out.println("stackMin，push:"+e);
+        }
+//            else if(e.compareTo(stackMin.peek())>0)
+//                stackMin.push(stackMin.peek());
+
+        if (stackMax.isEmpty() || e.compareTo(stackMax.peek()) > 0) {
+//                System.out.println("stackMax，push:"+e);
+            stackMax.push(e);
+        }
+//            else if(e.compareTo(stackMax.peek())<0)
+//                stackMin.push(stackMax.peek());
+    }
+
+    public E pop()//一定要记着，非空才能pop()
+    {
+        if (!stack1.isEmpty() && !stackMin.isEmpty() && !stackMax.isEmpty()) {
+            E e = stack1.pop();
+            if (e == stackMin.peek())
+                stackMin.pop();
+            if (e == stackMax.peek())
+                stackMax.pop();
+            return e;
+        } else {
+            System.out.println("栈已经为空了");
+            return null;
+        }
+
+    }
+
+    public E getMin() {
+        E result = null;
+        if (!stackMin.isEmpty()) result = stackMin.peek();
+        return result;
+    }
+
+    public E getMax() {
+        E result = null;
+        if (!stackMax.isEmpty()) result = stackMax.peek();
+        return stackMax.peek();
+    }
+//        public E getMed()
+//        {
+//            E[] ss=stack1.toArray(new E[stack1.size()]);//stack1.toArray()返回的是Object[],  栈----->数组;不能toArray后再强制转换，会报错
+//            Arrays.sort(ss);
+//            return ss[ss.length/2];
+//        }
+
+}
