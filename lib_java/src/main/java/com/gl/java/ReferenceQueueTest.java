fa10795d41f1e765a11972673cfd2fc3c9f579b7
leigao9 <leigao9@iflytek.com>
2022-11-27 17:31:47 +0800

    refactor:精简目录

---
+++ lib_java/src/main/java/com/gl/java/ReferenceQueueTest.java
@@ -0,0 +1,16 @@
+package com.gl.java;
+
+import android.text.SpannableString;
+import android.util.Log;
+
+import java.lang.ref.PhantomReference;
+import java.lang.ref.Reference;
+import java.lang.ref.ReferenceQueue;
+
+public class ReferenceQueueTest {
+
+        private static ReferenceQueue<Object> rq = new ReferenceQueue<Object>();
+        public static void main(String[] args){
+
+        }
+    }
